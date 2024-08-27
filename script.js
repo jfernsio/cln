f7dd2d5f86cf047e298880130c213da7bb922a01
Joel Fernandes <150249488+jfernsio@users.noreply.github.com>
2024-08-27 13:35:27 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,6 @@
+// img = "71294f43bd9ebfb1ded42e02432bfc79.jpg"
+
+// For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.
+// For a B2B software company, we developed an SEO strategy that resulted in a first page ranking for key keywords and a 200% increase in organic traffic.
+// Learn more
+// Learn 
